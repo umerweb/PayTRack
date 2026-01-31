323cba83abd68d9f980c26382c2ea1fcd8c983af
UMer <umer.web2@gmail.com>
2026-01-31 15:58:29 +0500

    added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'PayTrack',
+  webDir: 'dist'
+};
+
+export default config;
